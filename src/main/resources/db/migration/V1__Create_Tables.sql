f5b00de87230302d7ddd1c4c240145af007dae47
azfrank <49417746+azfrank@users.noreply.github.com>
2020-07-06 13:20:52 +0200

    Risolto errore sql

--- src/main/resources/db/migration/V1__Create_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Tables.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS course (
 	id UUID PRIMARY KEY,
 	course_title varchar(255) ,
 	user_nick varchar(255) ,
-	max_numeric_of_participants int(3),
-	number_of_actual_participants int(3),
-    creation_course_date Date,
-    last_change_date Date,
+	max_numeric_of_participants int,
+	number_of_actual_participants int,
+    creation_course_date DATE,
+    last_change_date DATE,
     status varchar(255)
-);
+)
